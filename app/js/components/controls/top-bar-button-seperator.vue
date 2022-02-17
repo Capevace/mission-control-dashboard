4106736c04c09f40771ba5d9705d29bc4a536989
Lukas Mateffy <github@mateffy.me>
2022-02-17 04:02:17 +0100

    Compat version

--- app/js/components/controls/top-bar-button-seperator.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<div class="top-bar-button-seperator"></div>
-</template>
-<style lang="scss">
-	.top-bar-button-seperator {
-		@apply border-r-2 border-purple-800 h-full;
-	}
-</style>
